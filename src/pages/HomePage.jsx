e50983853adb14efd32e9821aad115bd222f5803
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-01-03 19:22:02 +0000

    Progress.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -22,7 +22,7 @@ function HomePage() {
             <SwiperSlide className='w-screen h-screen relative' id='first'>
                 <section className='absolute bottom-10 left-0 p-5 text-white'>
                     <p className=" text-inter uppercase lg:text-2xl my-2">East Legon</p>
-                    <h1 className='text-5xl lg:text-8xl  w-screen text-bn uppercase tracking-widest my-2'>Urban Platinum Hotel</h1>
+                    <h1 className='text-5xl lg:text-8xl text-bn uppercase tracking-widest my-2'>Urban Platinum Hotel</h1>
                     <Link>
                                     <div className='border border-white w-[100px] h-[45px] rounded px-4 py-2 hover:scale-105 ease duration-150 relative'>
                                         <div className='bg-black w-full h-full absolute top-0 left-0 rounded opacity-50'></div>
@@ -36,7 +36,7 @@ function HomePage() {
             <SwiperSlide className='w-screen h-screen relative' id='second'>
                 <section className='absolute bottom-10 left-0 p-5 text-white'>
                     <p className=" text-inter uppercase lg:text-2xl my-2">Airport Residential</p>
-                    <h1 className='text-5xl lg:text-8xl  w-screen text-bn uppercase tracking-widest my-2'>Ghana National Museum</h1>
+                    <h1 className='text-5xl lg:text-8xl text-bn uppercase tracking-widest my-2'>Ghana National Museum</h1>
                    <Link>
                                     <div className='border border-white w-[100px] h-[45px] rounded px-4 py-2  hover:scale-105 ease duration-150 relative'>
                                         <div className='bg-black w-full h-full absolute top-0 left-0 rounded opacity-50'></div>
@@ -48,9 +48,9 @@ function HomePage() {
                 </section>
             </SwiperSlide>
             <SwiperSlide className='w-screen h-screen relative' id='third'>
-                <section className='absolute bottom-10 left-0 p-5 text-white'>
+                <section className='absolute bottom-10 left-0 text-white p-5'>
                     <p className=" text-inter uppercase lg:text-2xl my-2">Tema Comm. 25</p>
-                    <h1 className='text-5xl lg:text-8xl  w-screen text-bn uppercase tracking-widest my-2'>ARX Luxury Apartments</h1>
+                    <h1 className='text-5xl lg:text-8xl text-bn uppercase tracking-widest my-2'>ARX Luxury Apartments</h1>
                    <Link>
                                     <div className='border border-white w-[100px] h-[45px] rounded px-4 py-2 hover:scale-105 ease duration-150 relative'>
                                         <div className='bg-black w-full h-full absolute top-0 left-0 rounded opacity-50'></div>
