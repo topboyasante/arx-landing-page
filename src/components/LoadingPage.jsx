fa5e64fb9b09518c301728eb17fcf14bbbffa31a
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-01-04 17:48:09 +0000

    changes

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function LoadingPage() {
   return (
-    <div>LoadingPage</div>
+    <main className='w-screen h-screen bg-[#1B1925]'>
+        <h1 className="text-bn text-6xl text-center">Loading...</h1>
+    </main>
   )
 }
 
